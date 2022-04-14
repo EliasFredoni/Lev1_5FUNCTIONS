ec922a716304a63860db4f110fad9364b8ed515c
EliasFredoni <fredoni.elias@gmail.com>
2022-04-14 14:32:26 +0200

    finish

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+function math() {
+    console.log(30 * 20);
+    console.log(30 / 20);
+    console.log(100 * 100);
+    console.log(100 / 100);
+    console.log(5 * 0);
+    console.log(5 / 0);
+    console.log(45 * 173);
+    console.log(45 / 173);
+    console.log(1 * 1000);
+    console.log(1 / 1000);
+}
+math(10, 2);
